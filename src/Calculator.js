6bf63da7d056ac71a0a4e0635cac89ea047483cb
Alena Sharai <exultantislupus@gmail.com>
2024-10-18 17:50:54 +0400

    fix: play sound when duration changes

--- src/Calculator.js
+++ src/Calculator.js
@@ -9,27 +9,23 @@ const Calculator = ({ workouts, allowSound }) => {
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
 
-  const playSound = useCallback(() => {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  }, [allowSound]);
-
   useEffect(() => {
     setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, [number, sets, speed, durationBreak]);
+
+  useEffect(() => {
+    const playSound = () => {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    };
 
     playSound();
-  }, [number, sets, speed, durationBreak, playSound]);
+  }, [duration, allowSound]);
 
-  const handleInc = () => {
-    playSound();
-    setDuration((duration) => Math.floor(duration) + 1);
-  };
-
-  const handleDec = () => {
-    playSound();
+  const handleInc = () => setDuration((duration) => Math.floor(duration) + 1);
+  const handleDec = () =>
     setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
-  };
 
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
